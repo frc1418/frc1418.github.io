b41b111826435af55551f99ef4f14472cd30e00d
ErikBoesen <me@erikboesen.com>
2016-06-13 14:26:06 -0400

    Lots of improvements

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
+window.onscroll = function() {
+    document.getElementsByClassName('video-container')[0].style.transform = 'translateY(' + document.body.scrollTop / 1.5 + 'px)';
+};
 document.getElementById('email').onclick = function() {
     window.open('mailto:ballouj@fccps.org');
-};
-window.onscroll = function() {
-    document.getElementsByTagName('video')[0].style.transform = 'translateY(' + document.body.scrollTop / 1.5 + 'px)';
 };
