df22f5598727615fe3921c35b0faf983bf7ccb2d
ErikBoesen <me@erikboesen.com>
2016-06-13 22:21:35 -0400

    Added mobile theme, and optimized content. Production-ready, but needs lots of optimizations et al.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
-if (window.innerWidth >= 800) {
+// Mobile?
+if (window.innerWidth >= 850) {
+    // Give video its source, will initiate load. Otherwise, it won't load anything and will be hidden by CSS.
     document.getElementsByTagName('video')[0].src = 'video.mp4';
 
-    setInterval(function() {
-        if (banners.parentElement.querySelector(':hover') === banners) {
-            banners.scrollLeft += 1;
-        }
-    }, 20);
+    // Parallax effect for video background.
+    window.onscroll = function() {
+        document.getElementsByClassName('video-container')[0].style.transform = 'translateY(' + document.body.scrollTop / 1.5 + 'px)';
+    };
 }
-window.onscroll = function() {
-    document.getElementsByClassName('video-container')[0].style.transform = 'translateY(' + document.body.scrollTop / 1.5 + 'px)';
-};
 banners = document.getElementsByClassName('banners')[0];
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 document.getElementById('email').onclick = function() {
-    window.open('mailto:ballouj@fccps.org');
+	window.open('mailto:ballouj@fccps.org');
+};
+
+var button = document.getElementById('nav-button');
+var nav = document.getElementsByTagName('nav')[0];
+button.onclick = function() {
+	if (button.className === 'open') {
+		button.className = '';
+        nav.className = '';
+	} else {
+		button.className = 'open';
+        nav.className = 'open';
+	}
 };
