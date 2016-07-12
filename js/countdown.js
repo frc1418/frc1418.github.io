00a47a8c29d33f7dc3c89937305e0ee74b7f757c
ErikBoesen <me@erikboesen.com>
2016-07-12 10:04:46 -0400

    Revisions to countdown clock

--- js/countdown.js
+++ js/countdown.js
@@ -1,43 +1,47 @@
-var events = [
-    {
-        name: 'ILITE Robotics Offseason Challenge',
-        date: new Date('Oct 22, 2016')
-    }
-];
+var events = [{
+	name: 'I.R.O.C.',
+	date: new Date('Oct 22, 2016')
+}];
 
 var clock = {
-    title: document.getElementById('title'),
-    d: document.getElementById('days'),
-    hr: document.getElementById('hours'),
-    min: document.getElementById('minutes'),
-    s: document.getElementById('seconds'),
-    ms: document.getElementById('milliseconds')
+	title: document.getElementById('title'),
+	d: document.getElementById('days'),
+	hr: document.getElementById('hours'),
+	min: document.getElementById('minutes'),
+	s: document.getElementById('seconds'),
+	ms: document.getElementById('milliseconds')
 };
 
 // variables for time units
 var days, hours, minutes, seconds;
 
-// update the tag with id "countdown" every 1 second
-setInterval(function () {
+// Update countdown every 100ms (.1s)
+setInterval(function() {
+	// Find the amount of seconds between now and target
+	var now = new Date().getTime();
+	var secondsLeft = (events[0].date - now) / 1000;
+	if (now > events[0].date) {
+		secondsLeft = 0;
+	}
 
-    // Find the amount of seconds between now and target
-    var now = new Date().getTime();
-    var secondsLeft = (events[0].date - now) / 1000;
+	// Calculate times left
+	days = parseInt(secondsLeft / 86400);
+	if (days.toString().length < 2) days = '0' + days.toString();
+	secondsLeft = secondsLeft % 86400;
 
-    // do some time calculations
-    days = parseInt(secondsLeft / 86400);
-    secondsLeft = secondsLeft % 86400;
+	hours = parseInt(secondsLeft / 3600);
+	if (hours.toString().length < 2) hours = '0' + hours.toString();
+	secondsLeft = secondsLeft % 3600;
 
-    hours = parseInt(secondsLeft / 3600);
-    secondsLeft = secondsLeft % 3600;
+	minutes = parseInt(secondsLeft / 60);
+	if (minutes.toString().length < 2) minutes = '0' + minutes.toString();
+	seconds = parseInt(secondsLeft % 60);
+	if (seconds.toString().length < 2) seconds = '0' + seconds.toString();
 
-    minutes = parseInt(secondsLeft / 60);
-    seconds = parseInt(secondsLeft % 60);
-
-    // Display times left
-    clock.title.innerHTML = 'Time left until ' + events[0].name + ':';
-    clock.d.innerHTML = days;
-    clock.hr.innerHTML = hours;
-    clock.min.innerHTML = minutes;
-    clock.s.innerHTML = seconds;
-}, 10);
+	// Display times left
+	clock.title.innerHTML = 'Time left until ' + events[0].name + ':';
+	clock.d.innerHTML = days;
+	clock.hr.innerHTML = hours;
+	clock.min.innerHTML = minutes;
+	clock.s.innerHTML = seconds;
+}, 100);
