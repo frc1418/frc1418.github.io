1f50e367855df5ab703f3e5c487eedb06bb4200a
ErikBoesen <me@erikboesen.com>
2016-08-16 11:05:07 -0400

    Add Google Analytics

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,13 @@ for (i = 0; i < links.length; i++) {
     // If any of them contain instances of "ATSYMBOL" or "PERIOD" replace them as appropriate.
 	links[i].href = links[i].href.replace(/ATSYMBOL/, '@').replace(/PERIOD/, '.');
 }
+
+if (location.protocol === 'http:' || location.protocol === 'https:') {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-82626035-1', 'auto');
+    ga('send', 'pageview');
+}
