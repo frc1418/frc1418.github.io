a9ca0d76b90d08ee5b3653893df6888c2a13cd62
ErikBoesen <me@erikboesen.com>
2016-09-07 17:01:06 -0400

    Homepage background video is now a gif image

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 // Is device mobile?
 if (window.innerWidth >= 850) {
-    // Give video its source, will initiate load. Otherwise, it won't load anything and will be hidden by CSS.
-    // This is so the large video doesn't burn mobile data on phones.
-    // Also it's a stylistic nightmare.
-    document.getElementsByTagName('video')[0].src = 'video.mp4';
-
     // Parallax effect for video background.
     window.onscroll = function() {
-        document.getElementsByClassName('video-container')[0].style.transform = 'translateY(' + document.body.scrollTop / 1.5 + 'px)';
+        document.getElementById('bg').style.transform = 'translateY(' + document.body.scrollTop / -1.5 + 'px)';
     };
 }
