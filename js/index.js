dd79d2fbe8bb0616330693286413876405a01a0c
ErikBoesen <me@erikboesen.com>
2016-10-17 13:12:02 -0400

    Prefiltered video, optimized images

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ if (window.innerWidth >= 850) {
     // Give video its source, will initiate load. Otherwise, it won't load anything and will be hidden by CSS.
     // This is so the video doesn't burn mobile data on phones.
     // Also it's a stylistic nightmare.
-    document.getElementsByTagName('video')[0].src = 'video.mp4';
+    document.getElementsByTagName('video')[0].src = 'video-blur.mp4';
 
     // Parallax effect for video background.
     window.onscroll = function() {
