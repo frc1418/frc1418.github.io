d427d88a2de46e22ad710d99aafcbe9b2eeaca63
aderhall <muzioclavecin@gmail.com>
2017-02-11 13:42:25 -0500

    add 2017 robot to robots page

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ if (window.innerWidth >= 850) {
 var greetings = ['Ngiyakwemukela', 'Kíimak \'oolal', ' 	Ẹ ku abọ', 'Märr-ŋamathirri', 'Siya namkela nonke', 'ברוך־הבא', 'Merhbe', 'Croeso', 'Benvnuwe', 'Tere tulõmast', 'Hoan nghênh', 'Tervhen tuldes', 'Benvignùo', 'Xush kelibsiz', 'خوش آمديد', 'Ласкаво просимо', 'yI\'el', 'fi\'i', 'Xush kelibsiz', 'Hoş geldin', 'Maeva', 'Welkom', 'Willkumme', 'Bienveniu', 'Mwaiseni', 'Maogmáng Pag-abót', 'Welkam', 'Emedi', 'Willkommen', 'Tikilluarit', 'Aloha', 'Wayakurua', 'Selamat Datang', 'Benvenuto', 'Mauri', 'Murakaza neza', 'B’a’ntulena', 'Velkommen', 'Bainvegni', 'Бзиала шәаабеит', 'Шъукъеблагъ', 'Welkom', 'ḫaṣānu', 'Mayad-ayad nga pad-abot', 'Mir se vjên', 'Mirë se vjen', 'Qaĝaasakung huzuu haqakux̂', 'Wellkumma', 'እንኳን ደህና መጣህ።', 'أهلاً و سهلاً', 'Ahla w sahla', 'Bienvenius', 'Բարի գալուստ', 'Բարի՜ եկաք:', 'Ghini vinit', 'আদৰণি', 'Bienveníu', 'Miro peicak', 'Xoş gəlmişsiniz', 'Salamat datang', 'Сәләм бирем', 'Ongi etorri', 'Menjuah-juah', 'Še das d\' kemma bisd', 'Прывiтанне', 'Mwaiseni', 'স্বাগতম', 'आईं ना', 'Maogmáng Pag-abót', 'Welkam', 'Dobrodošli', 'Degemer mat', 'Добре дошъл', 'ကြိုဆိုပါတယ်', 'Benvingut', 'Maayong pag-abot', 'Kopisanangan do kinorikatan', 'Bienvenida', 'Bienvenidos', 'Bien binidu', 'Bienbenidu', 'Марша дагIийла шу', 'ᎤᎵᎮᎵᏍᏗ', '歡迎', '歡迎', '歡迎光臨', '欢迎', '歡迎光臨', 'Hóʔą', 'Boolkhent', 'Kopivosian do kinoikatan', 'Dynnargh dhywgh', 'Tatawaw', 'Malipayeng Pag-abot', 'Buďte vítáni', 'Velkommen', 'Welkom', 'Emedi', 'Tere tulemast', 'Miawoe zɔ', 'Vælkomin', 'Bula', 'Tervetuloa', 'Bienvenue', 'Wäljkiimen', 'Wäilkuumen', 'Wolkom', 'Agradît', 'Benvido', 'Buiti achüluruni', 'Adiu', 'Willkommen', 'კეთილი იყოს თქვენი', 'Ἀσπάζομαι!', 'Creoso a\'baramin' , 'Welcome']
 var counter = 0;
 setInterval(function() {
-    document.getElementById('welcome').innerHTML = greetings[Math.floor(counter*greetings.length)] + '!';
-    counter += 1/greetings.length;
+    document.getElementById('welcome').innerHTML = greetings[Math.floor(counter*greetings.length)];
+    counter += 1/(greetings.length+1);
     counter = (counter > 1) ? 0 : counter
-}, 2000);
+}, 3000);
