de73e306914bde99fcfd8cc3f27f70226bdbf5ca
ErikBoesen <me@erikboesen.com>
2017-04-15 14:29:07 -0400

    # This is a combination of 6 commits. # This is the 1st commit message: # This is a combination of 8 commits. # This is the 1st commit message: Fix spelling issues, close #103
    # This is the commit message #2:
    
    Add Andrew Blenke's profile photo.
    
    # This is the commit message #3:
    
    Remove seniors and those who've moved from Team page and update intro on homepage
    
    # This is the commit message #4:
    
    Make me not a team leader
    
    # This is the commit message #5:
    
    Edit member bios
    
    # This is the commit message #6:
    
    Add portraits for Sarah, Tahaseen, and Sofia
    
    # This is the commit message #7:
    
    HTML5 standardization - change <article> to <main>
    
    # This is the commit message #8:
    
    Switch to original, unsmoothed logo
    
    # This is the commit message #9:
    
    Add Branding page
    
    # This is the commit message #10:
    
    Update 2017, 2016, and home pages
    
    # This is the commit message #1:
    
    Update robot description pages
    
    # This is the commit message #2:
    
    Remove Emma Foley and Tristan Charpentier from the team page (#115)
    
    # This is the commit message #3:
    
    Add content skip button for ADA §508 conformity
    
    Partially resolve #117
    
    # This is the commit message #4:
    
    Make header animation better.
    
    # This is the commit message #5:
    
    Make icon return to more accurate postion.
    
    # This is the commit message #6:
    
    Make scale down closer to original
    
    # This is the commit message #7:
    
    added alt tags for all pages but outreach
    
    # This is the commit message #8:
    
    test
    
    # This is the commit message #2:
    
    Changed tabindex
    
    # This is the commit message #3:
    
    Finished the alt tags on all pages.
    
    # This is the commit message #4:
    
    Fix header menu items and header image.
    
    # This is the commit message #5:
    
    Make position of nav fixed
    
    # This is the commit message #6:
    
    Missed an alt tag.

--- js/steampunk.js
+++ js/steampunk.js
@@ -1,3 +1,3 @@
 window.onscroll = function() {
-    document.getElementsByTagName('article')[0].style.backgroundPosition = ((document.body.scrollTop / 5) - 400) + '% ' + ((document.body.scrollTop / 1.5) + 400) + '%';
+    document.getElementsByTagName('main')[0].style.backgroundPosition = ((document.body.scrollTop / 5) - 400) + '% ' + ((document.body.scrollTop / 1.5) + 400) + '%';
 };
