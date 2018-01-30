269923431c39cb45661b7cb05305af32ae0aa822
NathanielHorne <natch12032001@gmail.com>
2018-01-30 13:09:44 -0500

    Remove other website in the website

--- frc1418.github.io/js/header.js
+++
@@ -1,9 +0,0 @@
-document.onscroll = function() {shrinkHeader()};
-header = document.getElementsByTagName('header')[0];
-function shrinkHeader() {
-    if (document.body.scrollTop > 50 && document.body.clientWidth >= 1200 || document.documentElement.scrollTop > 50 && document.body.clientWidth >= 1200) {
-        header.className = 'shrink';
-    } else if (document.body.clientWidth >= 1200){ 
-        header.className = '';
-    }
-}
