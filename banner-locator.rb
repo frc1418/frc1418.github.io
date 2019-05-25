47e3a094de3b58c3a0ab4e7591d704293b5be493
AndrewLester <alester220@gmail.com>
2019-05-25 10:32:45 -0400

    Get banners from first comp to present

--- banner-locator.rb
+++ banner-locator.rb
@@ -1,23 +1,24 @@
 require 'tbarb'
 require 'yaml'
 
+TEAM = 1418
 tba = TBA.new('AykWePEZPKTjZxW6y7MbiTEpTfUlWrszcX5QBpIDUEZPBCJydltvhfd88MsBXxdS')
 year = DateTime.now.year
 award_map = {}
-10.times do |count|
-  awards = tba.team_awards(1418, year)
+(year - 1991).times do |count|
+  awards = tba.team_awards(TEAM, year)
   award_list = []
   awards.each do |award|
     award_name = award['name'].split(' Award ')[0]
-    events = tba.team_events(1418, year, true)
+    events = tba.team_events(TEAM, year, true)
     event_name = ''
     events.each do |event|
       if event['key'] == award['event_key']
         event_name = event['city']
       end
     end
-    if event_name == ''
-      next
+    if event_name == nil
+      event_name = ''
     end
     award_name = event_name + ' ' + award_name
     award_list.push(award_name)
