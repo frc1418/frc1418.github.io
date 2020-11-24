4bab66f072996358c78e7c68a1d4bbb31b13c740
AndrewLester <alester220@gmail.com>
2020-11-24 16:27:45 -0500

    Add headerShrink constant and fix formatting

--- js/header.js
+++ js/header.js
@@ -1,29 +1,30 @@
+const headerShrinkWidth = 1275;
 header = document.getElementsByTagName('header')[0];
 
 function shrinkHeader() {
-    if ((document.documentElement.scrollTop || document.body.scrollTop) > 50 || document.body.clientWidth <= 1275) {
+    if (document.documentElement.scrollTop > 50 || document.body.clientWidth <= headerShrinkWidth) {
         header.className = 'shrink';
     } else {
         header.className = '';
     }
 }
 
-//Shrink header on mobile and resized window
+// Shrink header on mobile and resized window
 body.onload = function () {
-    if (body.clientWidth <= 1275) {
+    if (body.clientWidth <= headerShrinkWidth) {
         header.className = 'shrink';
     } else {
         header.className = '';
     }
 };
 body.onresize = function () {
-    if (body.clientWidth <= 1275) {
+    if (body.clientWidth <= headerShrinkWidth) {
         header.className = 'shrink';
     } else {
         header.className = '';
     }
 
-    if (body.clientWidth > 1275) {
+    if (body.clientWidth > headerShrinkWidth) {
         if (nav.className == 'open') {
             body.style.overflow = null;
         }
