0142c89a402b4544a4a20155fc3a3af788028d7f
AndrewLester <alester220@gmail.com>
2020-11-24 16:02:11 -0500

    Move social icons to nav panel on mobile

--- js/header.js
+++ js/header.js
@@ -1,9 +1,37 @@
-document.onscroll = function() {shrinkHeader()};
 header = document.getElementsByTagName('header')[0];
+
 function shrinkHeader() {
-    if (document.body.scrollTop > 50 && document.body.clientWidth >= 1200 || document.documentElement.scrollTop > 50 && document.body.clientWidth >= 1200) {
+    if ((document.documentElement.scrollTop || document.body.scrollTop) > 50 || document.body.clientWidth <= 1275) {
         header.className = 'shrink';
-    } else if (document.body.clientWidth >= 1200){ 
+    } else {
         header.className = '';
     }
 }
+
+//Shrink header on mobile and resized window
+body.onload = function () {
+    if (body.clientWidth <= 1275) {
+        header.className = 'shrink';
+    } else {
+        header.className = '';
+    }
+};
+body.onresize = function () {
+    if (body.clientWidth <= 1275) {
+        header.className = 'shrink';
+    } else {
+        header.className = '';
+    }
+
+    if (body.clientWidth > 1275) {
+        if (nav.className == 'open') {
+            body.style.overflow = null;
+        }
+    } else {
+        if (nav.className == 'open') {
+            body.style.overflow = 'hidden';
+        }
+    }
+};
+
+document.addEventListener('scroll', shrinkHeader);
