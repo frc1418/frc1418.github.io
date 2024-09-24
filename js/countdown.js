d5f51190c56fecb1eb63597f24bc0370159af17a
CJ Artz <118136127+1ceit@users.noreply.github.com>
2024-09-24 15:34:55 -0400

    2024-2025 Update (#617)
    * 404 Page moblie update 1.1
    
    * FIxed Issude 455
    
    * temp fix for #538
    
    * Fix banner multiple rows
    
    * Delete Unneeded comments in 404.html & main.sass
    
    * Add Countdown to Index
    
    * countdown fix, add 1 sponsor, update team roster and use yml, add a company var list to alumi,
    
    * add back original key and fix spelling issue
    
    * add new team image
    
    * logic fix

--- js/countdown.js
+++ js/countdown.js
@@ -1,33 +1,18 @@
+// Array of events with the event name and date
 var events = [
-	{
-		name: 'Stop Build Day',
-		date: new Date(2018, 1, 20, 23, 59)
-	},
-	{
-    	name: 'CHS District Northern Virginia Event',
-    	date: new Date(2018, 2, 2, 11, 0)
-	},
-	{
-    	name: 'CHS District Greater DC Event co-sponsored by Micron',
-    	date: new Date(2018, 2, 9, 11, 0)
-	},
-	{
-    	name: 'FIRST Chesapeake District Championship',
-    	date: new Date(2018, 2, 28, 11, 0)
-	},
-	{
-    	name: 'FIRST Championship (Detroit)',
-    	date: new Date(2018, 3, 25)
-	}
+    {
+        name: '2025 FIRST Robotics Competition Kickoff',
+        date: new Date(2025, 0, 4, 12, 0) // (year, month - 1 (jan is 0 dec is 11), day, hour, minute)s
+    }
 ];
 
 var clock = {
-	title: document.getElementById('title'),
-	d: document.getElementById('days'),
-	hr: document.getElementById('hours'),
-	min: document.getElementById('minutes'),
-	s: document.getElementById('seconds'),
-	ms: document.getElementById('milliseconds')
+    title: document.querySelector('#countdown-section h1'),
+    d: document.getElementById('days'),
+    hr: document.getElementById('hours'),
+    min: document.getElementById('minutes'),
+	s: document.getElementById('seconds')
+	// Removed milliseconds it's not used
 };
 
 // variables for time units
@@ -36,30 +21,41 @@ var days, hours, minutes, seconds;
 // Update countdown every 100ms (.1s)
 setInterval(() => {
 	// Find the amount of seconds between now and target
-	var now = new Date().getTime();
-	var secondsLeft = (events[0].date - now) / 1000;
-	if (now > events[0].date) {
-		secondsLeft = 0;
+    var now = new Date().getTime();
+    var secondsLeft = (events[0].date - now) / 1000;
+    if (now > events[0].date) {
+        secondsLeft = 0;
 	}
-
+	
 	// Calculate times left
 	days = parseInt(secondsLeft / 86400);
-	if (days.toString().length < 2) days = '0' + days.toString();
+    if (days.toString().length < 2) days = '0' + days.toString();
 	secondsLeft = secondsLeft % 86400;
 
 	hours = parseInt(secondsLeft / 3600);
-	if (hours.toString().length < 2) hours = '0' + hours.toString();
+    if (hours.toString().length < 2) hours = '0' + hours.toString();
 	secondsLeft = secondsLeft % 3600;
 
 	minutes = parseInt(secondsLeft / 60);
-	if (minutes.toString().length < 2) minutes = '0' + minutes.toString();
+    if (minutes.toString().length < 2) minutes = '0' + minutes.toString();
 	seconds = parseInt(secondsLeft % 60);
-	if (seconds.toString().length < 2) seconds = '0' + seconds.toString();
+    if (seconds.toString().length < 2) seconds = '0' + seconds.toString();
 
-	// Display times left
-	clock.title.textContent = 'Time left until ' + events[0].name + ':';
-	clock.d.textContent = days;
-	clock.hr.textContent = hours;
-	clock.min.textContent = minutes;
+    	// Display times left
+    clock.title.textContent = events[0].name +':';
+    clock.d.textContent = days;
+    clock.hr.textContent = hours;
+    clock.min.textContent = minutes;
 	clock.s.textContent = seconds;
+
+	if (days === '00' && hours === '00' && parseInt(minutes) < 10) {
+        document.querySelector('#countdown-section').classList.add('flashing');
+    } else {
+        document.querySelector('#countdown-section').classList.remove('flashing');
+    }
 }, 100);
+
+	//Add :
+clock.d.insertAdjacentHTML('afterend', '<span>:</span>');
+clock.hr.insertAdjacentHTML('afterend', '<span>:</span>');
+clock.min.insertAdjacentHTML('afterend', '<span>:</span>');
