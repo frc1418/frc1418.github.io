70ba37f0a93ed2693e3657cbcd95b86cf9a3da8e
CJ Artz <c.j.a.34a@gmail.com>
2024-12-19 19:01:49 -0500

    Fix countdown & header blur

--- js/countdown.js
+++ js/countdown.js
@@ -1,61 +1,64 @@
-// Array of events with the event name and date
 var events = [
     {
         name: '2025 FIRST Robotics Competition Kickoff',
-        date: new Date(2025, 0, 4, 12, 0) // (year, month - 1 (jan is 0 dec is 11), day, hour, minute)s
+        date: new Date(2025, 0, 4, 12, 0) // (year, month - 1 (jan is 0 dec is 11), day, hour, minutes)
     }
 ];
 
 var clock = {
+    section: document.getElementById('countdown-section'),
     title: document.querySelector('#countdown-section h1'),
     d: document.getElementById('days'),
     hr: document.getElementById('hours'),
     min: document.getElementById('minutes'),
-	s: document.getElementById('seconds')
-	// Removed milliseconds it's not used
+    s: document.getElementById('seconds')
 };
 
-// variables for time units
 var days, hours, minutes, seconds;
+var i = 0;
 
-// Update countdown every 100ms (.1s)
-setInterval(() => {
-	// Find the amount of seconds between now and target
+const start = setInterval(() => {
+    // Find the amount of seconds between now and event
     var now = new Date().getTime();
-    var secondsLeft = (events[0].date - now) / 1000;
-    if (now > events[0].date) {
+    var secondsLeft = (events[i].date - now) / 1000;
+    if (now > events[i].date) {
         secondsLeft = 0;
-	}
-	
-	// Calculate times left
-	days = parseInt(secondsLeft / 86400);
-    if (days.toString().length < 2) days = '0' + days.toString();
-	secondsLeft = secondsLeft % 86400;
+        if (i < events.length - 1) {
+            i++;
+        } else {
+            clearInterval(start)
+            clock.section.style.display = 'none';
+        }
+    }
 
-	hours = parseInt(secondsLeft / 3600);
-    if (hours.toString().length < 2) hours = '0' + hours.toString();
-	secondsLeft = secondsLeft % 3600;
+    // Calculate times left
+    days = parseInt(secondsLeft / 86400);
+    days = days.toString().padStart(2, 0);
+    secondsLeft = secondsLeft % 86400;
 
-	minutes = parseInt(secondsLeft / 60);
-    if (minutes.toString().length < 2) minutes = '0' + minutes.toString();
-	seconds = parseInt(secondsLeft % 60);
-    if (seconds.toString().length < 2) seconds = '0' + seconds.toString();
+    hours = parseInt(secondsLeft / 3600);
+    hours = hours.toString().padStart(2, 0);
+    secondsLeft = secondsLeft % 3600;
 
-    	// Display times left
-    clock.title.textContent = events[0].name +':';
+    minutes = parseInt(secondsLeft / 60);
+    minutes = minutes.toString().padStart(2, 0);
+
+    seconds = parseInt(secondsLeft % 60);
+    seconds = seconds.toString().padStart(2, 0);
+
+    clock.title.textContent = events[i].name + ':';
     clock.d.textContent = days;
     clock.hr.textContent = hours;
     clock.min.textContent = minutes;
-	clock.s.textContent = seconds;
+    clock.s.textContent = seconds;
 
-	if (days === '00' && hours === '00' && parseInt(minutes) < 10) {
+    if (days === '00' && parseInt(hours) < 1) {
         document.querySelector('#countdown-section').classList.add('flashing');
     } else {
         document.querySelector('#countdown-section').classList.remove('flashing');
     }
-}, 100);
+}, 1000);
 
-	//Add :
 clock.d.insertAdjacentHTML('afterend', '<span>:</span>');
 clock.hr.insertAdjacentHTML('afterend', '<span>:</span>');
 clock.min.insertAdjacentHTML('afterend', '<span>:</span>');
